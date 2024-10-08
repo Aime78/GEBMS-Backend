fdafb4f4e4203f6d95de9c702b7668bd5dc3e955
Rukundo Aime <rukundoaime78@gmail.com>
2024-10-08 15:18:55 +0200

    first commit

---
+++ src/main/java/com/gebms/gebmsbackend/GebmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gebms.gebmsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GebmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GebmsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gebms/gebmsbackend/GebmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gebms.gebmsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GebmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
